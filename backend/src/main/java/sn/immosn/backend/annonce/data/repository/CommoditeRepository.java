b25c385d499e50c9e5cf0efb7304ce05ff6b794b
Isaacdackey <isaacdackeyuniv@gmail.com>
2026-05-14 11:42:52 +0000

    Ajout de la méthode findByIsArchivedFalse avec pagination dans CommoditeRepository

--- backend/src/main/java/sn/immosn/backend/annonce/data/repository/CommoditeRepository.java
+++ backend/src/main/java/sn/immosn/backend/annonce/data/repository/CommoditeRepository.java
@@ -2,6 +2,8 @@ package sn.immosn.backend.annonce.data.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,5 @@ public interface CommoditeRepository extends JpaRepository<Commodite, Long> {
     List<Commodite> findByIsArchivedFalse();
     List<Commodite> findByIdInAndIsArchivedFalse(List<Long> ids);
     boolean existsByLibelleIgnoreCase(String libelle);
+    Page<Commodite> findByIsArchivedFalse(Pageable pageable);
 }
