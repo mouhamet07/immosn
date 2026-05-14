4ee87fec6980c0991f062e82e77172e230ceede5
Isaacdackey <isaacdackeyuniv@gmail.com>
2026-05-14 11:36:24 +0000

    Changement du return du type List getAllCommoditesPaged en Page<CommoditeResponseDto>

--- backend/src/main/java/sn/immosn/backend/annonce/service/CommoditeService.java
+++ backend/src/main/java/sn/immosn/backend/annonce/service/CommoditeService.java
@@ -2,6 +2,7 @@ package sn.immosn.backend.annonce.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import sn.immosn.backend.client.web.annonce.dto.CommoditeRequestDto;
@@ -10,7 +11,7 @@ import sn.immosn.backend.client.web.annonce.dto.CommoditeResponseDto;
 public interface CommoditeService {
     CommoditeResponseDto createCommodite(CommoditeRequestDto request);
     List<CommoditeResponseDto> getAllCommodites();             // actives seulement
-    List<CommoditeResponseDto> getAllCommoditesPaged(Pageable pageable);
+    Page<CommoditeResponseDto> getAllCommoditesPaged(Pageable pageable);
     CommoditeResponseDto getCommoditeById(Long id);
     CommoditeResponseDto updateCommodite(Long id, CommoditeRequestDto request);
     void archiveCommodite(Long id);
